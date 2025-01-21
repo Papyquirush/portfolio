34ef2ebbbf9e7c42e782975e8591e8f4cd9ec145
p2207127 <florian.dussud@etu.univ-lyon1.fr>
2025-01-21 23:14:33 +0100

    feat(navHeader): navBar

--- src/components/partials/NavHeader.vue
+++ src/components/partials/NavHeader.vue
@@ -1,18 +1,25 @@
 <template>
-<header>
-  <div class="wrapper">
-    <img class="logo" src="/logos/logo.png" alt="logo" />
-    <h1>
-      You did it!
-    </h1>
-  </div>
-</header>
+  <header>
+    <nav class="bg-gray-800 p-4 w-full">
+      <div class="container mx-auto flex flex-wrap justify-center items-center">
+        <div class="flex items-center">
+          <img src="/logos/logo.png" alt="Logo" class="h-12 w-21 mr-2">
+          <div class="text-white text-lg font-bold">Mon Portfolio</div>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="text-sm lg:flex-grow text-center">
+            <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4">Home</a>
+            <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4">Skills</a>
+            <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white">Contact Me</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
-
 </style>
