298edaa24dff8c60151402151a03b04dac43e65a
Florian DUSSUD <137182759+Papyquirush@users.noreply.github.com>
2025-01-21 15:24:45 +0100

    feat(navHeader): nav + logo

---
+++ src/components/partials/NavHeader.vue
@@ -0,0 +1,18 @@
+<template>
+<header>
+  <div class="wrapper">
+    <img class="logo" src="/logos/logo.png" alt="logo" />
+    <h1>
+      You did it!
+    </h1>
+  </div>
+</header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
