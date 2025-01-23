c7f1ad9d5e217a8373e66d2c1fc1bf252fa7dbce
Florian DUSSUD <137182759+Papyquirush@users.noreply.github.com>
2025-01-23 11:32:46 +0100

    feat(translate): traduction + ajout bouton changement de langue dans navHeader

--- src/components/partials/NavHeader.vue
+++ src/components/partials/NavHeader.vue
@@ -4,7 +4,7 @@
       <div class="container mx-auto flex flex-wrap justify-center items-center">
         <div class="flex items-center">
           <img src="/logos/logo.png" alt="Logo" class="h-12 w-21 mr-2">
-          <div class="text-white text-lg font-bold">Mon Portfolio</div>
+          <div class="text-white text-lg font-bold">{{ $t('portfolio') }}</div>
         </div>
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="text-sm lg:flex-grow flex ml-20">
@@ -17,9 +17,23 @@
                 @mouseover="link.isHovered = true"
                 @mouseleave="link.isHovered = false"
             >
-              {{ link.name }}
+              {{ $t(link.name) }}
             </a>
           </div>
+        </div>
+        <div
+            class="relative w-16 h-8 bg-gray-600 rounded-full flex items-center justify-between p-1 cursor-pointer"
+            @click="changeLanguage"
+        >
+
+          <div
+              class="w-6 h-6 bg-white rounded-full transform transition-transform duration-300 ease-in-out"
+              v-bind:class="locale === 'fr' ? 'translate-x-8' : 'translate-x-0'"
+          >
+
+          </div>
+          <span class="absolute left-2 text-xs font-bold text-gray-800">EN</span>
+          <span class="absolute right-2 text-xs font-bold text-gray-800">FR</span>
         </div>
       </div>
     </nav>
@@ -28,15 +42,22 @@
 
 <script setup>
 import { reactive } from "vue";
+import { useI18n } from 'vue-i18n';
+const { locale } = useI18n();
 
 const links = reactive([
-  { name: "Home", isHovered: false,l:"/" },
-  { name: "My Projects", isHovered: false,l:"/projects" },
-  { name: "Skills", isHovered: false,l:"/skills"},
-  { name: "Experiences", isHovered: false,l:"/experiences" },
-  { name: "About Me", isHovered: false,l:"/about" },
-  { name: "Contact Me", isHovered: false,l:"/contact" },
+  {name: "home", isHovered: false, l: "/"},
+  {name: "myProjects", isHovered: false, l: "/projects"},
+  {name: "skills", isHovered: false, l: "/skills"},
+  {name: "experiences", isHovered: false, l: "/experiences"},
+  {name: "aboutMe", isHovered: false, l: "/about"},
+  {name: "contactMe", isHovered: false, l: "/contact"},
 ]);
+
+function changeLanguage() {
+  locale.value = locale.value === 'en' ? 'fr' : 'en';
+  console.log(locale.value);
+}
 </script>
 
 <style scoped>
@@ -47,4 +68,5 @@ const links = reactive([
   font-weight: bold;
   transition: ease-in-out 0.5s;
 }
+
 </style>
