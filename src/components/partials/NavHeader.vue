fbc03175f0bea72c4e459fd045c693337760ffba
Florian DUSSUD <137182759+Papyquirush@users.noreply.github.com>
2025-01-23 10:12:50 +0100

    feat(navHeader): cahngement liens + logo + style

--- src/components/partials/NavHeader.vue
+++ src/components/partials/NavHeader.vue
@@ -7,10 +7,18 @@
           <div class="text-white text-lg font-bold">Mon Portfolio</div>
         </div>
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div class="text-sm lg:flex-grow text-center">
-            <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4">Home</a>
-            <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4">Skills</a>
-            <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white">Contact Me</a>
+          <div class="text-sm lg:flex-grow flex ml-20">
+            <a
+                v-for="link in links"
+                :key="link.name"
+                :href="link.l"
+                class="w-full text-gray-300 mr-4"
+                :class="{ navLink: link.isHovered }"
+                @mouseover="link.isHovered = true"
+                @mouseleave="link.isHovered = false"
+            >
+              {{ link.name }}
+            </a>
           </div>
         </div>
       </div>
@@ -19,7 +27,25 @@
 </template>
 
 <script setup>
+import { reactive } from "vue";
+
+// Liste des liens avec leur état de survol
+const links = reactive([
+  { name: "Home", isHovered: false,l:"/" },
+  { name: "My Projects", isHovered: false,l:"/projects" },
+  { name: "Skills", isHovered: false,l:"/skills"},
+  { name: "Experiences", isHovered: false,l:"/experiences" },
+  { name: "About Me", isHovered: false,l:"/about" },
+  { name: "Contact Me", isHovered: false,l:"/contact" },
+]);
 </script>
 
 <style scoped>
+.navLink {
+  text-decoration: underline;
+  text-decoration-thickness: 2px;
+  text-underline-offset: 4px;
+  color: aliceblue;
+  font-weight: bold;
+}
 </style>
