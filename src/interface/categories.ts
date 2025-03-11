05787e555ba0055f6850f67ea606bb8031ecfafc
Florian DUSSUD <137182759+Papyquirush@users.noreply.github.com>
2025-03-11 17:19:46 +0100

    feat(skills): skills percentages

--- src/interface/categories.ts
+++ src/interface/categories.ts
@@ -5,13 +5,13 @@ export const categories = {
             title: "backend",
             description: "Symfony, MySQL, Java...",
             skills: [
-                { name: "PHP", level: 80, logo: "/logos/back/php.svg" },
-                { name: "Symfony", level: 75, logo: "/logos/back/symfony.svg" },
-                { name: "CakePHP", level: 70, logo: "/logos/back/cakephp.svg" },
-                { name: "Angular", level: 65, logo: "/logos/back/angular.svg" },
-                { name: "MySQL", level: 85, logo: "/logos/back/mysql.svg" },
-                { name: "Java", level: 60, logo: "/logos/back/java.svg" },
-                { name: "C++", level: 55, logo: "/logos/back/c++.svg" }
+                { name: "PHP", level: 30, logo: "/logos/back/php.svg" },
+                { name: "MySQL", level: 30, logo: "/logos/back/mysql.svg" },
+                { name: "Java", level: 30, logo: "/logos/back/java.svg" },
+                { name: "Symfony", level: 20, logo: "/logos/back/symfony.svg" },
+                { name: "C++", level: 10, logo: "/logos/back/c++.svg" },
+                { name: "CakePHP", level: 5, logo: "/logos/back/cakephp.svg" },
+                { name: "Angular", level: 5, logo: "/logos/back/angular.svg" }
             ]
         },
         {
@@ -19,13 +19,13 @@ export const categories = {
             title: "frontend",
             description: "TypeScript, Vue.js, Tailwind CSS ...",
             skills: [
-                { name: "JavaScript", level: 90, logo: "/logos/front/javascript.svg" },
-                { name: "TypeScript", level: 85, logo: "/logos/front/typescript.svg" },
-                { name: "Vue.js", level: 80, logo: "/logos/front/vue.svg" },
-                { name: "Tailwind CSS", level: 75, logo: "/logos/front/tailwind.svg" },
-                { name: "Bootstrap", level: 70, logo: "/logos/front/bootstrap.svg" },
-                { name: "HTML", level: 95, logo: "/logos/front/html.svg" },
-                { name: "CSS", level: 90, logo: "/logos/front/css.svg" }
+                { name: "HTML", level: 75, logo: "/logos/front/html.svg" },
+                { name: "CSS", level: 60, logo: "/logos/front/css.svg" },
+                { name: "JavaScript", level: 30, logo: "/logos/front/javascript.svg" },
+                { name: "TypeScript", level: 30, logo: "/logos/front/typescript.svg" },
+                { name: "Tailwind CSS", level: 20, logo: "/logos/front/tailwind.svg" },
+                { name: "Vue.js", level: 15, logo: "/logos/front/vue.svg" },
+                { name: "Bootstrap", level: 5, logo: "/logos/front/bootstrap.svg" },
             ]
         },
         {
@@ -33,17 +33,17 @@ export const categories = {
             title: "tools",
             description: "Git, Docker, Talend...",
             skills: [
-                { name: "GitHub", level: 85, logo: "/logos/tools/github.svg" },
-                { name: "GitLab", level: 80, logo: "/logos/tools/gitlab.svg" },
-                { name: "Docker", level: 75, logo: "/logos/tools/docker.svg" },
-                { name: "Talend", level: 70, logo: "/logos/tools/talend.svg" },
-                { name: "VS Code", level: 90, logo: "/logos/tools/vscode.svg" },
-                { name: "JetBrains Tools", level: 85, logo: "/logos/tools/jetbrains.svg" },
-                { name: "Android Studio", level: 80, logo: "/logos/tools/android.svg" },
-                { name: "Postman", level: 75, logo: "/logos/tools/postman.svg" },
+                { name: "JetBrains Tools", level: 80, logo: "/logos/tools/jetbrains.svg" },
+                { name: "VS Code", level: 70, logo: "/logos/tools/vscode.svg" },
+                { name: "Postman", level: 70, logo: "/logos/tools/postman.svg" },
                 { name: "Office Suite", level: 70, logo: "/logos/tools/office.svg" },
-                { name: "Inkscape", level: 65, logo: "/logos/tools/inkscape.svg" },
-                { name: "QT Creator", level: 60, logo: "/logos/tools/qt.svg" }
+                { name: "GitHub", level: 60, logo: "/logos/tools/github.svg" },
+                { name: "GitLab", level: 60, logo: "/logos/tools/gitlab.svg" },
+                { name: "Android Studio", level: 60, logo: "/logos/tools/android.svg" },
+                { name: "Inkscape", level: 60, logo: "/logos/tools/inkscape.svg" },
+                { name: "QT Creator", level: 50, logo: "/logos/tools/qt.svg" },
+                { name: "Talend", level: 30, logo: "/logos/tools/talend.svg" },
+                { name: "Docker", level: 15, logo: "/logos/tools/docker.svg" },
             ]
         }
     ]
