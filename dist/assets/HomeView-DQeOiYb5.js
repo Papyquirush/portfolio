e47073a0b7fff79e01084c24055baefb1b342d74
Florian DUSSUD <137182759+Papyquirush@users.noreply.github.com>
2025-03-13 17:35:04 +0100

    feat(prod): node modules + dist

--- dist/assets/HomeView-DQeOiYb5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-JERdnAXp.js";const t={},n={class:"content"};function s(a,_){return o(),c("main",n)}const d=e(t,[["render",s],["__scopeId","data-v-ae913cb0"]]);export{d as default};
