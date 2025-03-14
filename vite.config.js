380eceba88b4f5f66a5fe5a4c9a72987339f3eb8
Florian DUSSUD <137182759+Papyquirush@users.noreply.github.com>
2025-03-14 12:26:09 +0100

    feat(prod): config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
 export default defineConfig({
   base: './',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',
       'vue': 'vue/dist/vue.esm-bundler.js'
     }
   },
