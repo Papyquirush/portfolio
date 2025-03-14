321faed7b974f494cf17cccb95673aa5e8334467
Florian DUSSUD <137182759+Papyquirush@users.noreply.github.com>
2025-03-14 08:59:07 +0100

    feat(prod): htaccess + vite config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
-import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': '/src',
       'vue': 'vue/dist/vue.esm-bundler.js'
-    },
+    }
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 });
