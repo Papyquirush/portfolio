83deb3f83a74a9b3378b349e4d0385800a9251e3
p2207127 <florian.dussud@etu.univ-lyon1.fr>
2025-03-16 13:45:04 +0100

    feat(prod): Site en construction

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,14 @@
 <template>
-
-<main class="content">
-<h1>Home</h1>
-</main>
-
+  <main class="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-4">
+    <h1 class="text-4xl font-bold text-gray-800 mb-4">Site en construction</h1>
+    <p class="text-lg text-gray-600">Revenez bientôt !</p>
+  </main>
 </template>
 
 <script setup>
 
-
 </script>
 
-
 <style scoped>
-.content{
-
-}
 
 </style>
